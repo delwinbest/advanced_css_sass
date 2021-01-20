bb5e46c2ff91c9c039c5c9113e34f8123eeff076
Delwin Best <mail@delwinbest.com>
2021-01-21 08:13:39 +0900

    Button rounding and animation/transistion

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ function App() {
     <div className={classes.App}>
       <header className={classes.header}>
         <div className={classes.logoBox}>
-          <img src={logo} alt="Logo" className={classes.logo}/>
+          <img   src={logo} alt="Logo" className={classes.logo}/>
         </div>
         <div className={classes.textBox}> 
           <h1 className={classes.headingPrimary}>
             <span className={classes.headingPrimaryMain}>Outdoors</span>
             <span className={classes.headingPrimarySub}>is where life happens</span>
           </h1>
+          <a href="/#" className={classes.ButtonWhite}>Discover our tours</a>
         </div>
 
       </header>
