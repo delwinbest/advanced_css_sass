78d4598c28e77103985a120d2b9535932ba58bfe
Delwin Best <mail@delwinbest.com>
2021-01-22 09:06:28 +0900

    restructure based on ownership. adjust mobile padding

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
         <div className={classes.logo__Box}>
           <img   src={logo} alt="Logo" className={classes.logo}/>
         </div>
-        <div className={classes.header__textBox}> 
-          <h1 className={classes.headingPrimary}>
-            <span className={classes.headingPrimary__Main}>Outdoors</span>
-            <span className={classes.headingPrimary__Sub}>is where life happens</span>
+        <div className={classes.header_textBox}> 
+          <h1 className={classes.header_textBox_headingPrimary}>
+            <span className={classes.header_textBox_headingPrimary__Main}>Outdoors</span>
+            <span className={classes.header_textBox_headingPrimary__Sub}>is where life happens</span>
           </h1>
           <a href="/#" className={classes.ButtonWhite}>Discover our tours</a>
         </div>
