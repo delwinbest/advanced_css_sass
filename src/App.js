0de0613956b5a7b87aa860c9fbb4bb5675567d1d
Delwin Best <mail@delwinbest.com>
2021-01-23 10:53:46 +0900

    switched away from class based cscc

--- src/App.js
+++ src/App.js
@@ -1,88 +1,95 @@
 import logo from './img/logo-white.png';
-import * as classes from './sass/App.module.scss';
+// import * as classes from './sass/App.module.scss';
+import './sass/App.scss';
 
 function App() {
   return (
-    <div className={classes.App}>
-      <header className={classes.header}>
-        <div className={classes.logo__Box}>
-          <img   src={logo} alt="Logo" className={classes.logo}/>
+    <div className="App">
+      <header className="header">
+        <div className="logo__Box">
+          <img   src={logo} alt="Logo" className="logo"/>
         </div>
-        <div className={classes.header_textBox}> 
-          <h1 className={classes.header_textBox_headingPrimary}>
-            <span className={classes.header_textBox_headingPrimary__Main}>Outdoors</span>
-            <span className={classes.header_textBox_headingPrimary__Sub}>is where life happens</span>
+        <div className="header_textBox"> 
+          <h1 className="header_textBox_headingPrimary">
+            <span className="header_textBox_headingPrimary__Main">Outdoors</span>
+            <span className="header_textBox_headingPrimary__Sub">is where life happens</span>
           </h1>
-          <a href="/#" className={classes.ButtonWhite}>Discover our tours</a>
+          <a href="/#" className="ButtonWhite">Discover our tours</a>
         </div>
       </header>
 
       <main>
-        <div className={classes.sectionAbout}>
-          <div className={classes.u_centerText}>
-            <h2 className={classes.headingSecondary}>Exciting tours for adventurous people</h2>
+        <div className="sectionAbout">
+          <div className="u_centerText">
+            <h2 className="headingSecondary u_marginBotom__8">Exciting tours for adventurous people</h2>
+          </div>
+          <div className="gridTest_Col__1_of_2">
+            Col 1 of 2  
+          </div>
+          <div className="gridTest_Col__1_of_2">
+            Col 1 of 2  
           </div>
         </div>
       </main>
 
-      {/* <section className={classes.gridTest}>
-        <div className={classes.gridTest_Row}>
-          <div className={classes.gridTest_Col__1_of_2}>
+      {/* <section className={gridTest}>
+        <div className={gridTest_Row}>
+          <div className={gridTest_Col__1_of_2}>
             Col 1 of 2  
           </div>
-          <div className={classes.gridTest_Col__1_of_2}>
+          <div className={gridTest_Col__1_of_2}>
             Col 1 of 2  
           </div>
         </div>
-        <div className={classes.gridTest_Row}>
-          <div className={classes.gridTest_Col__1_of_3}>
+        <div className={gridTest_Row}>
+          <div className={gridTest_Col__1_of_3}>
             Col 1 of 3  
           </div>
-          <div className={classes.gridTest_Col__1_of_3}>
+          <div className={gridTest_Col__1_of_3}>
             Col 1 of 3  
           </div>
-          <div className={classes.gridTest_Col__1_of_3}>
+          <div className={gridTest_Col__1_of_3}>
             Col 1 of 3  
           </div>
         </div>
-        <div className={classes.gridTest_Row}>
-          <div className={classes.gridTest_Col__1_of_3}>
+        <div className={gridTest_Row}>
+          <div className={gridTest_Col__1_of_3}>
             Col 1 of 3  
           </div>
-          <div className={classes.gridTest_Col__2_of_3}>
+          <div className={gridTest_Col__2_of_3}>
             Col 2 of 3  
           </div>
         </div>
-        <div className={classes.gridTest_Row}>
-          <div className={classes.gridTest_Col__1_of_4}>
+        <div className={gridTest_Row}>
+          <div className={gridTest_Col__1_of_4}>
             Col 1 of 4  
           </div>
-          <div className={classes.gridTest_Col__1_of_4}>
+          <div className={gridTest_Col__1_of_4}>
             Col 1 of 4  
           </div>
-          <div className={classes.gridTest_Col__1_of_4}>
+          <div className={gridTest_Col__1_of_4}>
             Col 1 of 4  
           </div>
-          <div className={classes.gridTest_Col__1_of_4}>
+          <div className={gridTest_Col__1_of_4}>
             Col 1 of 4  
           </div>
         </div>
-        <div className={classes.gridTest_Row}>
-          <div className={classes.gridTest_Col__1_of_4}>
+        <div className={gridTest_Row}>
+          <div className={gridTest_Col__1_of_4}>
             Col 1 of 4  
           </div>
-          <div className={classes.gridTest_Col__1_of_4}>
+          <div className={gridTest_Col__1_of_4}>
             Col 1 of 4  
           </div>
-          <div className={classes.gridTest_Col__2_of_4}>
+          <div className={gridTest_Col__2_of_4}>
             Col 2 of 4  
           </div>
         </div>
-        <div className={classes.gridTest_Row}>
-          <div className={classes.gridTest_Col__1_of_4}>
+        <div className={gridTest_Row}>
+          <div className={gridTest_Col__1_of_4}>
             Col 1 of 4  
           </div>
-          <div className={classes.gridTest_Col__3_of_4}>
+          <div className={gridTest_Col__3_of_4}>
             Col 3 of 4  
           </div>
         </div>
