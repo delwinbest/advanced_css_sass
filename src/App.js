6c3a82a61432dec8d8381aaf36050c94cf70b27a
Delwin Best <mail@delwinbest.com>
2021-01-23 09:25:33 +0900

    create rows, not last child and calc

--- src/App.js
+++ src/App.js
@@ -15,8 +15,69 @@ function App() {
           </h1>
           <a href="/#" className={classes.ButtonWhite}>Discover our tours</a>
         </div>
-
       </header>
+      <section className={classes.gridTest}>
+        <div className={classes.gridTest_Row}>
+          <div className={classes.gridTest_Col__1_of_2}>
+            Col 1 of 2  
+          </div>
+          <div className={classes.gridTest_Col__1_of_2}>
+            Col 1 of 2  
+          </div>
+        </div>
+        <div className={classes.gridTest_Row}>
+          <div className={classes.gridTest_Col__1_of_3}>
+            Col 1 of 3  
+          </div>
+          <div className={classes.gridTest_Col__1_of_3}>
+            Col 1 of 3  
+          </div>
+          <div className={classes.gridTest_Col__1_of_3}>
+            Col 1 of 3  
+          </div>
+        </div>
+        <div className={classes.gridTest_Row}>
+          <div className={classes.gridTest_Col__1_of_3}>
+            Col 1 of 3  
+          </div>
+          <div className={classes.gridTest_Col__2_of_3}>
+            Col 2 of 3  
+          </div>
+        </div>
+        <div className={classes.gridTest_Row}>
+          <div className={classes.gridTest_Col__1_of_4}>
+            Col 1 of 4  
+          </div>
+          <div className={classes.gridTest_Col__1_of_4}>
+            Col 1 of 4  
+          </div>
+          <div className={classes.gridTest_Col__1_of_4}>
+            Col 1 of 4  
+          </div>
+          <div className={classes.gridTest_Col__1_of_4}>
+            Col 1 of 4  
+          </div>
+        </div>
+        <div className={classes.gridTest_Row}>
+          <div className={classes.gridTest_Col__1_of_4}>
+            Col 1 of 4  
+          </div>
+          <div className={classes.gridTest_Col__1_of_4}>
+            Col 1 of 4  
+          </div>
+          <div className={classes.gridTest_Col__2_of_4}>
+            Col 2 of 4  
+          </div>
+        </div>
+        <div className={classes.gridTest_Row}>
+          <div className={classes.gridTest_Col__1_of_4}>
+            Col 1 of 4  
+          </div>
+          <div className={classes.gridTest_Col__3_of_4}>
+            Col 3 of 4  
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
