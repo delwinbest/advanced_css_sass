b14ef4e31c9301df43ee7786375c2b9ee6b23761
Delwin Best <mail@delwinbest.com>
2021-02-03 13:53:55 +0900

    side nave create and flex direction column

--- src/App.js
+++ src/App.js
@@ -34,13 +34,51 @@ function App() {
               </div>
               <div className="user-nav__user">
                 <img src={user_img} alt="" className="user-nav__user-photo"/>
-                <span className="user-nav__user_name">Delwin Best</span>
+                <span className="user-nav__user_name">Delwin</span>
               </div>
             </nav>
           </header>
           <div className="content">
             <nav className="sidebar">
-              Navigation
+              <ul className="side-nav">
+                <li className="side-nav__item">
+                  <a href="/#" className="side-nav__link">
+                    <svg className="side-nav__icon">
+                      <use href={`${svg_sprite}#icon-home`}></use> 
+                    </svg>
+                    <span>Hotel</span>
+                  </a>
+                </li>
+                <li className="side-nav__item">
+                  <a href="/#" className="side-nav__link">
+                    <svg className="side-nav__icon">
+                      <use href={`${svg_sprite}#icon-aircraft-take-off`}></use> 
+                    </svg>
+                    <span>Flight</span>
+                  </a>
+                </li>
+                <li className="side-nav__item">
+                  <a href="/#" className="side-nav__link">
+                    <svg className="side-nav__icon">
+                      <use href={`${svg_sprite}#icon-key`}></use> 
+                    </svg>
+                    <span>Car Rental</span>
+                  </a>
+                </li>
+                <li className="side-nav__item">
+                  <a href="/#" className="side-nav__link">
+                    <svg className="side-nav__icon">
+                      <use href={`${svg_sprite}#icon-map`}></use> 
+                    </svg>
+                    <span>Tours</span>
+                  </a>
+                </li>
+              </ul>
+
+              <div className="legal">
+                &copy; 2017 by trillo. All rights reserved.
+              </div>
+
             </nav>
             <main className="hotel-view">
               Hotel View
