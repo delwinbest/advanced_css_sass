9f35fb646a7c3aa7f24e2960b5734dba22b6d75d
Delwin Best <mail@delwinbest.com>
2021-02-08 19:50:44 +0900

    overview bar

--- src/App.js
+++ src/App.js
@@ -95,6 +95,39 @@ function App() {
                   <img src={hotel_img_3} alt="Hotel 3" className="gallery__photo"/>
                 </figure>
               </div>
+              <div className="overview">
+                <h1 className="overview__heading">
+                  Hotel Las Palmas
+                </h1>
+                <div className="overview__stars">
+                    <svg className="overview__icon-star">
+                      <use href={`${svg_sprite}#icon-star`}></use> 
+                    </svg>
+                    <svg className="overview__icon-star">
+                      <use href={`${svg_sprite}#icon-star`}></use> 
+                    </svg>
+                    <svg className="overview__icon-star">
+                      <use href={`${svg_sprite}#icon-star`}></use> 
+                    </svg>
+                    <svg className="overview__icon-star">
+                      <use href={`${svg_sprite}#icon-star`}></use> 
+                    </svg>
+                    <svg className="overview__icon-star">
+                      <use href={`${svg_sprite}#icon-star`}></use> 
+                    </svg>
+                </div>
+                <div className="overview__location">
+                  <svg className="overview__icon-location">
+                      <use href={`${svg_sprite}#icon-location-pin`}></use> 
+                  </svg>
+                  <button className="btn-inline">Albufeira, Portugal</button>
+                </div>
+
+                <div className="overview__rating">
+                  <div className="overview__rating-average">8.6</div>
+                  <div className="overview__rating-count">429 votes</div>
+                </div>
+              </div>
             </main>
           </div>
         </div>
