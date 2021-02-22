f48c9ddf95e23fd0c7ffa5ade1f62c540c8173de
Delwin Best <mail@delwinbest.com>
2021-02-22 19:52:36 +0900

    button scroll animation

--- src/App.js
+++ src/App.js
@@ -194,6 +194,17 @@ function App() {
                   <button className="btn-inline">Show all <span>→</span></button>
                 </div>
               </div>
+
+              <div className="cta">
+                  <h2 className="cta__book-now">
+                    Good news! We have 4 free rooms for your selected dates!
+                  </h2>
+                  <button className="btn">
+                    <span className="btn__visible">Book Now</span>
+                    <span className="btn__invisible">Only 4 rooms left</span>
+                    </button>
+              </div>
+
             </main>
           </div>
         </div>
