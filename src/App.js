24ad21701e1ee79cdc9345547978a39e3f641603
Delwin Best <mail@delwinbest.com>
2021-02-22 15:50:24 +0900

    ddd

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './sass/main.scss';
 import logo from './img/logo.png';
 import svg_sprite from './img/sprite.svg';
 import user_img from './img/user.jpg';
+import user_3 from './img/user-3.jpg';
+import user_4 from './img/user-4.jpg';
+import user_5 from './img/user-5.jpg';
+import user_6 from './img/user-6.jpg';
 import hotel_img_1 from './img/hotel-1.jpg';
 import hotel_img_2 from './img/hotel-2.jpg';
 import hotel_img_3 from './img/hotel-3.jpg';
@@ -126,6 +130,40 @@ function App() {
                 <div className="overview__rating">
                   <div className="overview__rating-average">8.6</div>
                   <div className="overview__rating-count">429 votes</div>
+                </div>
+              </div>
+              <div className="detail">
+                <div className="description">
+                  <p className="paragraph">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi nisi dignissimos debitis ratione sapiente saepe. Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate.
+                  </p>
+                  <p className="paragraph">
+                    Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate delectus, inventore iure aliquid aliquam.
+                  </p>
+                  <ul className="list">
+                    <li className="list__item">Close to the beach</li>
+                    <li className="list__item">Breakfast included</li>
+                    <li className="list__item">Free airport shuttle</li>
+                    <li className="list__item">Free wifi in all rooms</li>
+                    <li className="list__item">Air conditioning and heating</li>
+                    <li className="list__item">Pets allowed</li>
+                    <li className="list__item">We speak all languages</li>
+                    <li className="list__item">Perfect for families</li>
+                  </ul>
+                  <div className="recommend">
+                    <p className="recommend__count">
+                      Lucy and 3 other friends recommend this hotel.
+                    </p>
+                    <div className="recommend__friends">
+                      <img src={user_3} alt="Friend 1" className="recommend__photo"/>
+                      <img src={user_4} alt="Friend 2" className="recommend__photo"/>
+                      <img src={user_5} alt="Friend 3" className="recommend__photo"/>
+                      <img src={user_6} alt="Friend 4" className="recommend__photo"/>
+                    </div>
+                  </div>
+                </div>
+                <div className="user-reviews">
+                  User Reviews
                 </div>
               </div>
             </main>
