d1adc7b86732dd0282930be424c92a21655c3c1c
Delwin Best <mail@delwinbest.com>
2021-02-28 20:46:49 +0900

    grid in a grid

--- src/App.js
+++ src/App.js
@@ -17,7 +17,12 @@ function App() {
         </div>
 
         <section className="features">
-          Features
+          <div className="feature">feature 1</div>
+          <div className="feature">feature 2</div>
+          <div className="feature">feature 3</div>
+          <div className="feature">feature 4</div>
+          <div className="feature">feature 5</div>
+          <div className="feature">feature 6</div>
         </section>
 
         <div className="story__pictures">
