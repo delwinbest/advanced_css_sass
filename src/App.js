24dce3cb06bec854f285d577b96710ca1a28177b
Delwin Best <mail@delwinbest.com>
2021-03-04 16:18:17 +0900

    gallery complete

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ import imgHouse4 from './img/house-4.jpeg';
 import imgHouse5 from './img/house-5.jpeg';
 import imgHouse6 from './img/house-6.jpeg';
 
+import imgGallery1 from './img/gal-1.jpeg';
+import imgGallery2 from './img/gal-2.jpeg';
+import imgGallery3 from './img/gal-3.jpeg';
+import imgGallery4 from './img/gal-4.jpeg';
+import imgGallery5 from './img/gal-5.jpeg';
+import imgGallery6 from './img/gal-6.jpeg';
+import imgGallery7 from './img/gal-7.jpeg';
+import imgGallery8 from './img/gal-8.jpeg';
+import imgGallery9 from './img/gal-9.jpeg';
+import imgGallery10 from './img/gal-10.jpeg';
+import imgGallery11 from './img/gal-11.jpeg';
+import imgGallery12 from './img/gal-12.jpeg';
+import imgGallery13 from './img/gal-13.jpeg';
+import imgGallery14 from './img/gal-14.jpeg';
+
 function App() {
   return (
     <div className="container">
@@ -299,7 +314,50 @@ function App() {
         </section>
 
         <section className="gallery">
-          Gallery
+          <figure className="gallery__item gallery__item--1">
+            <img src={imgGallery1} alt="Gallery 1" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--2">
+            <img src={imgGallery2} alt="Gallery 2" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--3">
+            <img src={imgGallery3} alt="Gallery 2" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--4">
+            <img src={imgGallery4} alt="Gallery 4" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--5">
+            <img src={imgGallery5} alt="Gallery 5" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--6">
+            <img src={imgGallery6} alt="Gallery 6" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--7">
+            <img src={imgGallery7} alt="Gallery 7" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--8">
+            <img src={imgGallery8} alt="Gallery 8" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--9">
+            <img src={imgGallery9} alt="Gallery 9" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--10">
+            <img src={imgGallery10} alt="Gallery 10" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--11">
+            <img src={imgGallery11} alt="Gallery 11" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--12">
+            <img src={imgGallery12} alt="Gallery 12" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--13">
+            <img src={imgGallery13} alt="Gallery 13" className="gallery__img"/>
+          </figure>
+          <figure className="gallery__item gallery__item--14">
+            <img src={imgGallery14} alt="Gallery 14" className="gallery__img"/>
+          </figure>
+
+ 
         </section>
 
         <footer className="footer">
