7c31e0f0d7b95e624ce206a8cccdbcb7a59dfe38
Delwin Best <mail@delwinbest.com>
2021-03-05 14:02:02 +0900

    footer

--- src/App.js
+++ src/App.js
@@ -361,7 +361,18 @@ function App() {
         </section>
 
         <footer className="footer">
-          Footer
+          <ul className="nav">
+            <li className="nav__item"><a href="/#" className="nav__link">Find your dream home</a></li>
+            <li className="nav__item"><a href="/#" className="nav__link">Request proposal</a></li>
+            <li className="nav__item"><a href="/#" className="nav__link">Download home planner</a></li>
+            <li className="nav__item"><a href="/#" className="nav__link">Contact Us</a></li>
+            <li className="nav__item"><a href="/#" className="nav__link">Submit your property</a></li>
+            <li className="nav__item"><a href="/#" className="nav__link">Come work with us</a></li>
+          </ul>
+
+          <p className="copyright">
+            &copy; Copyright 2021 by Delwin Best. Feel free to use this project for your own purposes. This does not apply if you plan to produce your own course of tutorials.
+          </p>
         </footer>
     </div>
   );
