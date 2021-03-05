f474068861c4e3915872489cbc401a838e52e525
Delwin Best <mail@delwinbest.com>
2021-03-05 15:04:24 +0900

    realtors section done

--- src/App.js
+++ src/App.js
@@ -31,6 +31,10 @@ import imgLogoBI from './img/logo-bi.png'
 import imgLogoFORBES from './img/logo-forbes.png'
 import imgLogoTECHCRUNCH from './img/logo-techcrunch.png'
 
+import imgRealtor1 from './img/realtor-1.jpeg';
+import imgRealtor2 from './img/realtor-2.jpeg';
+import imgRealtor3 from './img/realtor-3.jpeg';
+
 function App() {
   return (
     <div className="container">
@@ -53,7 +57,26 @@ function App() {
         </header>
 
         <div className="realtors">
-          Top 3 Realtors
+          <h3 className="heading-3">Top 3 Realtors</h3>
+          <div className="realtors__list">
+            <img src={imgRealtor1} alt="Realtor 1" className="realtors__img"/>
+            <div className="realtors__details">
+              <h4 className="heading-4 heading-4--light">Erik Feinman</h4>
+              <p className="realtors__sold">245 houses sold</p>
+            </div>
+
+            <img src={imgRealtor2} alt="Realtor 2" className="realtors__img"/>
+            <div className="realtors__details">
+              <h4 className="heading-4 heading-4--light">Kim Brown</h4>
+              <p className="realtors__sold">212 houses sold</p>
+            </div>
+
+            <img src={imgRealtor3} alt="Realtor 3" className="realtors__img"/>
+            <div className="realtors__details">
+              <h4 className="heading-4 heading-4--light">Toby Ramsey</h4>
+              <p className="realtors__sold">198 houses sold</p>
+            </div>
+          </div>
         </div>
 
         <section className="features">
